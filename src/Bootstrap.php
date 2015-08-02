b7d7061d85259c54a9a3206b8ce71f780744ccba
kiler129 <grzegorz@noflash.pl>
2015-08-02 23:53:17 +0200

    Added application bootstrap file.

---
+++ src/Bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace noFlash\PkiAuthenticator;
+
+define('APP_ROOT', realpath(dirname(__FILE__) . DIRECTORY_SEPARATOR . '..'));
+require_once(APP_ROOT . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php'); //Composer autoloader
+
+//Initialize config
+Config::getInstance(APP_ROOT . DIRECTORY_SEPARATOR . 'config.ini');
